0735a1f8d2b1f734c2cf227d1111eb0c2a13da4a
tosmo5 <tosmo5@163.com>
2022-10-25 10:35:12 +0800

    reflect

--- src/main/kotlin/com/tosmo/ktils/Reflecter.kt
+++ src/main/kotlin/com/tosmo/ktils/Reflecter.kt
@@ -1,104 +1,119 @@
 package com.tosmo.ktils
 
-import java.lang.reflect.Field
-import kotlin.reflect.*
-import kotlin.reflect.full.*
+import kotlin.reflect.KClass
+import kotlin.reflect.KParameter
+import kotlin.reflect.KProperty
+import kotlin.reflect.full.createType
+import kotlin.reflect.full.isSubtypeOf
 
 /**
  * 反射
  *
  * @author Thomas Miao
  */
-class Reflecter<T : Any>(val kClass: KClass<T>) {
+object Reflecter {
 
-    companion object {
-        /**
-         * 对比[property]是否为[typeClass]的类型
-         */
-        fun mergeType(property: KProperty<*>, typeClass: KClass<*>): Boolean {
-            return property.returnType.isSubtypeOf(typeClass.createType())
-        }
-
-        /**
-         * [property]的类型是否与[typeClasses]所有类型都匹配
-         */
-        fun mergeAllType(property: KProperty<*>, vararg typeClasses: KClass<*>): Boolean {
-            return typeClasses.all { property.returnType.isSubtypeOf(it.createType()) }
-        }
-
-        /**
-         * [property]是否与[typeClasses]中一个或多个类型匹配
-         */
-        fun mergeAnyType(property: KProperty<*>, vararg typeClasses: KClass<*>): Boolean {
-            return typeClasses.any { property.returnType.isSubtypeOf(it.createType()) }
-        }
+    /**
+     * 对比[property]是否为[typeClass]的类型
+     */
+    fun mergeType(property: KProperty<*>, typeClass: KClass<*>): Boolean {
+        return property.returnType.isSubtypeOf(typeClass.createType())
     }
 
     /**
-     * [kClass]中定义的所有属性集合，按照声明顺序排序
+     * [property]的类型是否与[typeClasses]所有类型都匹配
      */
-    val unsortedDeclaredMemberProperties: Collection<KProperty1<T, *>> = buildList {
-        kClass.java.declaredFields.map { field: Field ->
-            kClass.declaredMemberProperties.find { field.name == it.name }?.let { add(it) }
-        }
+    fun mergeAllType(property: KProperty<*>, vararg typeClasses: KClass<*>): Boolean {
+        return typeClasses.all { property.returnType.isSubtypeOf(it.createType()) }
     }
 
     /**
-     * [kClass]中定义的所有可变属性集合，按照声明顺序排序
+     * [property]是否与[typeClasses]中一个或多个类型匹配
      */
-    val unsortedVarDeclaredMemberProperties: Collection<KMutableProperty1<T, *>> =
-        unsortedDeclaredMemberProperties.filterIsInstance<KMutableProperty1<T, *>>()
-
-    /**
-     * 调用[T]的空构造函数创建一个对象，不存在空构建函数时返回空
-     */
-    fun createBean(): T {
-        require(kClass.constructors.any { it.parameters.isEmpty() }) { "${kClass.qualifiedName} → 找不到无参构造函数" }
-        return kClass.createInstance()
+    fun mergeAnyType(property: KProperty<*>, vararg typeClasses: KClass<*>): Boolean {
+        return typeClasses.any { property.returnType.isSubtypeOf(it.createType()) }
     }
 
     /**
-     * 通过[args]创建一个对象
-     *
-     * @param args 属性名与值的映射集合
+     * 用[args]中的参数，自动匹配一个合适的构造函数并创建一个[kClass]对象
      */
-    fun createBean(args: Map<String, *>): T {
-        kClass.constructors.find {
-            it.parameters.isEmpty()
+    fun <T : Any> createBean(kClass: KClass<T>, vararg args: Any?): T =
+        createBean(kClass, args.toList())
+
+    /**
+     * 用[args]中的参数，自动匹配一个合适的构造函数并创建一个[kClass]对象
+     */
+    fun <T : Any> createBean(kClass: KClass<T>, args: Collection<*>): T {
+        // 当没有给参数时，尝试调用无参构造
+        if (args.isEmpty()) {
+            val constructor = kClass.constructors.find {
+                it.parameters.isEmpty() || it.parameters.all { p -> p.isOptional }
+            }
+            requireNotNull(constructor) { "没有符合的构造函数" }
+            return constructor.callBy(emptyMap())
         }
-        val usedArgNames = mutableListOf<String>()
-        // 构造对象
-        val bean = if (kClass.constructors.any { it.parameters.isEmpty() }) {
-            kClass.createInstance()
-        } else {
-            val initFun = kClass.constructors.find { kFunction ->
-                kFunction.parameters.all { kParameter ->
-                    // 所给的属性值包含了构造函数的参数 或 构造函数的参数可为空
-                    kParameter.name in args || kParameter.type.isMarkedNullable
+        // 找出可传参数数量匹配的构造函数
+        val constructors = kClass.constructors.filter {
+            it.parameters.isEmpty() || it.parameters.size == args.size || (
+                    it.parameters.size > args.size && it.parameters.subList(
+                        args.size, it.parameters.size
+                    ).all { p -> p.isOptional })
+        }.toMutableList()
+        // 过滤掉参数类型不匹配的构造函数，并记录参数映射
+        val argsMap = mutableMapOf<KParameter, Any?>()
+        val constructor = constructors.filter { initFun ->
+            if (initFun.parameters.isEmpty())
+                return@filter true
+            for (i in args.indices) {
+                val param = initFun.parameters[i]
+                val arg = args.elementAt(i)
+                if (arg == null) {
+                    if (param.type.isMarkedNullable)
+                        argsMap.putIfAbsent(param, null)
+                    else
+                        return@filter false
+                } else {
+                    if (arg::class.createType().isSubtypeOf(param.type))
+                        argsMap.putIfAbsent(param, arg)
+                    else
+                        return@filter false
                 }
             }
-            requireNotNull(initFun) { "${kClass.qualifiedName} → 提供的参数不足以创建对象" }
-            buildMap {
-                initFun.parameters.forEach {
-                    val value = args[it.name]
-                    require(!it.type.isMarkedNullable && value == null) {
-                        "${kClass.qualifiedName}的参数${it.name}不可为空，但取到了空值"
-                    }
-                    put(it, value)
-                    usedArgNames.add(it.name!!)
-                }
-            }.let { initFun.callBy(it) }
+            true
+        }.let {
+            require(it.isNotEmpty()) { "没有符合的构造函数" }
+            it.firstOrNull { initFun -> initFun.parameters.isNotEmpty() }
+                ?: it.first()
         }
+        return constructor.callBy(argsMap)
+    }
 
-        // 设定非构造函数中的属性值
-        kClass.memberProperties.filter {
-            it.name in args && it.name !in usedArgNames
-        }.forEach {
-            if (it is KMutableProperty<*>) {
-                it.setter.call(bean, args[it.name])
+    /**
+     * 用[args]中的参数，自动匹配一个合适的构造函数并创建一个[kClass]对象
+     */
+    fun <T : Any> createBean(kClass: KClass<T>, args: Map<String, *>): T {
+        // 判断空构造函数
+        if (args.isEmpty()) {
+            val constructor = kClass.constructors.find {
+                it.parameters.isEmpty() || it.parameters.all { p -> p.isOptional }
+            }
+            requireNotNull(constructor) { "没有符合的构造函数" }
+            return constructor.callBy(emptyMap())
+        }
+        // 转换参数映射
+        val argsMap = buildMap {
+            kClass.constructors.forEach { initFun ->
+                initFun.parameters.forEach { kParam ->
+                    args[kParam.name]?.let { putIfAbsent(kParam, it) }
+                }
             }
         }
-
-        return bean
+        return kClass.constructors.filterNot { initFun ->
+            initFun.parameters.any { !(it in argsMap || it.isOptional) }
+        }.let {
+            require(it.isNotEmpty()) { "没有符合的构造函数" }
+            (it.firstOrNull { initFun -> initFun.parameters.isNotEmpty() }
+                ?: it.first()).callBy(argsMap)
+        }
     }
 }
